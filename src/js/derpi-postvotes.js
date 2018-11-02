76a432311539736ef6d05cc9285ad343634f47db
SeinopSys <seinopsys@gmail.com>
2018-11-02 02:50:04 +0100

    Add rate limiting

--- src/js/derpi-postvotes.js
+++ src/js/derpi-postvotes.js
@@ -6,10 +6,47 @@
 	const LOG_PREFIX = '[Derpi-PostVotes] ';
 	const PROCESSED_CLASS_NAME = 'dpv-processed';
 	const INSERTED_CLASS_NAME = 'dpv-inserted';
+	const DISABLED_CLASS_NAME = 'dpv-disabled';
+	const INTERACTION_CONTAINER_CLASS_NAME = 'dpv-interaction-container';
 	const API_KEY_SESSION_CACHE_KEY = 'apikey';
 
 	const prepend = (parent, el) => parent.insertBefore(el, parent.firstChild);
 	const findInteractionsContainer = comm => comm.querySelector('.communication__options').children[0].children[1];
+
+	class ExtensionStorage {
+		getItem(key) {
+			return new Promise(res => {
+				chrome.storage.local.get(key, items => res(items[key]));
+			});
+		}
+
+		setItem(key, value) {
+			return new Promise(res => {
+				chrome.storage.local.set({ [key]: value }, () => res());
+			});
+		}
+
+		removeItem(key) {
+			return new Promise(res => {
+				chrome.storage.local.remove(key, () => res());
+			});
+		}
+	}
+
+	const extensionStorage = new ExtensionStorage();
+
+	class StyleInjector {
+		constructor() {
+			this._element = document.createElement('style');
+			document.head.appendChild(this._element);
+		}
+
+		setContent(cssString) {
+			this._element.innerHTML = cssString;
+		}
+	}
+
+	const customCSS = new StyleInjector();
 
 	class Wrapper {
 		constructor() {
@@ -18,6 +55,12 @@
 			this.client = null;
 			this.user = null;
 			this.hookTimer = null;
+			this.limitTimer = null;
+			customCSS.setContent(`
+				.${DISABLED_CLASS_NAME} { opacity: .5; pointer-events: none }
+				.${INTERACTION_CONTAINER_CLASS_NAME} { display: flex; align-items: center; user-select: none; cursor: default }
+				.${INSERTED_CLASS_NAME}.communication__interaction { padding: 1px 2px; display: inline-block }
+			`);
 		}
 
 		init() {
@@ -74,6 +117,14 @@
 						});
 					});
 				});
+				this.client.on('vote-limit-reached', data => {
+					console.info(`${LOG_PREFIX}%cVote rate limit reached, votes cannot be cast by user %d for the next %d seconds`, 'color:#a55', this.user.id, data.allowVotingIn);
+					extensionStorage.setItem(this.getLimitKey(), new Date().getTime() + (data.allowVotingIn * 1000));
+					this.setLimitTimer();
+				});
+				this.client.on('rate-limit', data => {
+					alert(`This action is limited to ${data.threshold} per ${data.ttl} seconds. Please wait a bit and try again.`);
+				});
 			}).catch(err => {
 				console.error(err);
 				console.info(`${LOG_PREFIX}%cMust be logged in to vote`, 'color:blue');
@@ -83,24 +134,25 @@
 		fetchApiKey() {
 			const signedIn = document.querySelector('.js-datastore').dataset.userIsSignedIn === 'true';
 			if (!signedIn){
-				sessionStorage.removeItem(API_KEY_SESSION_CACHE_KEY);
+				extensionStorage.removeItem(API_KEY_SESSION_CACHE_KEY);
 				return Promise.reject('Not logged in');
 			}
 
-			const sessionSavedValue = sessionStorage.getItem(API_KEY_SESSION_CACHE_KEY);
-			if (sessionSavedValue)
-				return Promise.resolve(sessionSavedValue);
+			return extensionStorage.getItem(API_KEY_SESSION_CACHE_KEY).then(sessionSavedValue => {
+				if (sessionSavedValue)
+					return Promise.resolve(sessionSavedValue);
 
-			return fetch('/users/edit')
-				.then(r => r.text())
-				.then(r => {
-					const match = r.match(/<h3>API Key<\/h3><p>Your API key is <strong>([A-Za-z\d-]+)<\/strong>/);
-					if (!match)
-						return Promise.reject('API key missing');
+				return fetch('/users/edit')
+					.then(r => r.text())
+					.then(r => {
+						const match = r.match(/<h3>API Key<\/h3><p>Your API key is <strong>([A-Za-z\d-]+)<\/strong>/);
+						if (!match)
+							return Promise.reject('API key missing');
 
-					sessionStorage.setItem(API_KEY_SESSION_CACHE_KEY, match[1]);
-					return Promise.resolve(match[1]);
-				})
+						extensionStorage.setItem(API_KEY_SESSION_CACHE_KEY, match[1]);
+						return Promise.resolve(match[1]);
+					})
+			});
 		}
 
 		pollElements() {
@@ -126,8 +178,7 @@
 			this.client.emit('get-scores', { entities }, data => {
 				commsArray.forEach(comm => {
 					const interactionsContainer = findInteractionsContainer(comm);
-					interactionsContainer.style.display = 'flex';
-					interactionsContainer.style.alignItems = 'center';
+					interactionsContainer.classList.add(INTERACTION_CONTAINER_CLASS_NAME);
 					const score = data.scores[comm.id] || 0;
 					const userVote = data.userVotes[comm.id];
 
@@ -136,24 +187,63 @@
 					prepend(interactionsContainer, this.createScoreDisplay(score, comm.id, userVote));
 					prepend(interactionsContainer, this.createLink('Up', 'arrow-up', comm.id, userVote));
 				});
+				this.setLimitTimer();
 			});
 		}
 
-		destroyElements() {
+		forEachElement(f) {
 			const comms = document.querySelectorAll(`.block.communication.${PROCESSED_CLASS_NAME}`);
 			if (comms.length === 0)
 				return;
 
-			Array.from(comms).forEach(comm => {
+			Array.from(comms).forEach(f);
+		}
+
+		enableElements() {
+			this.forEachElement(comm => {
+				[
+					this.interactionEls.up[comm.id],
+					this.interactionEls.down[comm.id]
+				].forEach(el => {
+					if (el) el.classList.remove(DISABLED_CLASS_NAME);
+				});
+				const interactionsContainer = findInteractionsContainer(comm);
+				if (interactionsContainer)
+					interactionsContainer.title = undefined;
+			});
+		}
+
+		disableElements(date) {
+			this.forEachElement(comm => {
+				[
+					this.interactionEls.up[comm.id],
+					this.interactionEls.down[comm.id]
+				].forEach(el => {
+					if (el) el.classList.add(DISABLED_CLASS_NAME);
+				});
+				const interactionsContainer = findInteractionsContainer(comm);
+				if (interactionsContainer)
+					interactionsContainer.title = `You cast too many votes in a short period of time. Please wait until ${date.toLocaleTimeString()} before trying again.`;
+			});
+		}
+
+		destroyElements() {
+			this.forEachElement(comm => {
 				delete this.scoreDisplays[comm.id];
 				delete this.interactionEls.up[comm.id];
 				delete this.interactionEls.down[comm.id];
 
-				let inserted = comm.querySelectorAll(`.${INSERTED_CLASS_NAME}`);
+				const interactionsContainer = findInteractionsContainer(comm);
+				if (interactionsContainer)
+					interactionsContainer.classList.remove(INTERACTION_CONTAINER_CLASS_NAME);
+
+				let inserted = interactionsContainer.querySelectorAll(`.${INSERTED_CLASS_NAME}`);
 				Array.from(inserted).forEach(el => el.parentNode.removeChild(el));
 
 				clearInterval(this.hookTimer);
 				this.hookTimer = null;
+				clearInterval(this.limitTimer);
+				this.limitTimer = null;
 
 				comm.classList.remove(PROCESSED_CLASS_NAME);
 			});
@@ -165,8 +255,6 @@
 			const lcDirectionLabel = lcDirection + 'vote';
 			const el = document.createElement('a');
 			el.className = `${INSERTED_CLASS_NAME} communication__interaction interaction--${lcDirectionLabel}`;
-			el.style.display = 'inline-block';
-			el.style.padding = '1px 2px';
 			if (userVote === lcDirection)
 				el.className += ' active';
 			el.href = `#${lcDirectionLabel}`;
@@ -175,6 +263,9 @@
 			el.addEventListener('click', e => {
 				e.preventDefault();
 				e.stopPropagation();
+
+				if (el.classList.contains('disabled'))
+					return;
 
 				const direction = el.classList.contains('active') ? null : lcDirection;
 				this.client.emit('vote', { type, id, direction })
@@ -188,7 +279,6 @@
 				return;
 
 			const el = this.createInteractionElement(score, 'strong');
-			el.title = 'Score';
 			this.scoreDisplays[commId] = el;
 			return el;
 		}
@@ -198,6 +288,40 @@
 			el.className = `${INSERTED_CLASS_NAME} communication__interaction`;
 			el.innerHTML = html;
 			return el;
+		}
+
+		getLimitKey() {
+			return `${this.user.id}_limited_until`;
+		}
+
+		isLimited() {
+			return extensionStorage.getItem(this.getLimitKey()).then(untilTs => {
+				if (!untilTs)
+					return Promise.resolve(false);
+
+				const nowTs = new Date().getTime();
+				if (nowTs > untilTs)
+					return Promise.resolve(false);
+
+				return Promise.resolve({ untilTs, diff: untilTs - nowTs });
+			});
+		}
+
+		setLimitTimer() {
+			this.isLimited().then(data => {
+				if (data === false)
+					return;
+
+				if (this.limitTimer)
+					clearTimeout(this.limitTimer);
+				this.limitTimer = setTimeout(() => {
+					console.info(`${LOG_PREFIX}%cVoting rate limit refreshed for user %d`, 'color:deepskyblue', this.user.id);
+					this.enableElements();
+					this.limitTimer = null;
+				}, data.diff);
+				console.info(`${LOG_PREFIX}%cVoting rate limit for user %d will refresh in %f second(s)`, 'color:deepskyblue', this.user.id, data.diff / 1000);
+				this.disableElements(new Date(data.untilTs));
+			});
 		}
 	}
 
